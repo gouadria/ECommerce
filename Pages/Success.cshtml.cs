0740c9ea80a83a554c2e2ba759010bd16a73cf1c
gouadria <134183088+gouadria@users.noreply.github.com>
2025-04-13 01:36:34 +0200

    Update Success.cshtml.cs

--- Pages/Success.cshtml.cs
+++ Pages/Success.cshtml.cs
@@ -1,3 +1,12 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using ECommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
 public class SuccessModel : PageModel
 {
     private readonly EcommerceDbContext _context;
