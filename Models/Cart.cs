7c9a14ea36c2497b082134f3ad0e300101a43846
gouadria <134183088+gouadria@users.noreply.github.com>
2025-04-13 03:32:27 +0200

    Update Cart.cs

--- Models/Cart.cs
+++ Models/Cart.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -11,6 +11,7 @@ namespace ECommerce.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int CartId { get; set; }  // Identifiant du panier
+
         public string UserId { get; set; }  // Clé étrangère vers IdentityUser
         public DateTime CreatedDate { get; set; }
 
@@ -18,6 +19,12 @@ namespace ECommerce.Models
         public virtual List<CartProduct> CartProducts { get; set; } = new List<CartProduct>();
 
         public virtual IdentityUser User { get; set; }
+
+        // Constructeur pour garantir que les propriétés non-nullables sont initialisées
+        public Cart()
+        {
+            CartProducts = new List<CartProduct>();
+        }
     }
 
     public class CartProduct
@@ -26,14 +33,23 @@ namespace ECommerce.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int CartProductId { get; set; }
         public int CartId { get; set; }
+
+        // Propriété ProductName rendue nullable pour éviter l'erreur de non-nullabilité
+        public string? ProductName { get; set; }  // Nullable
+
         public int ProductId { get; set; }
-        public String ProductName { get; set; }
 
         public int Quantity { get; set; } // Stockage de la quantité ici
 
         // Relations
-        public virtual Cart Cart { get; set; }
-        public virtual Product Product { get; set; }
+        public virtual Cart Cart { get; set; }  // Rendre Cart nullable si nécessaire
+        public virtual Product? Product { get; set; }  // Rendre Product nullable si nécessaire
+
+        // Constructeur pour garantir que les propriétés non-nullables sont initialisées
+        public CartProduct()
+        {
+            ProductName = string.Empty;  // Initialisation à une valeur par défaut si nécessaire
+        }
     }
 }
 
