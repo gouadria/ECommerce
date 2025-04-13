b5632bd4b1c97e8df33764251952142ddfab347d
gouadria <134183088+gouadria@users.noreply.github.com>
2025-04-13 03:23:01 +0200

    Update ProductReview.cs

--- Models/ProductReview.cs
+++ Models/ProductReview.cs
@@ -14,7 +14,7 @@ public partial class ProductReview
 
     public int ProductId { get; set; }
 
-    public string UserId { get; set; }
+    public string? UserId { get; set; }
 
     public DateTime CreatedDate { get; set; }
 
