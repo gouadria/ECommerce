b19d9dd3cb2662424753367dc083c8ae3a07f031
gouadria <134183088+gouadria@users.noreply.github.com>
2025-04-17 23:58:10 +0200

    Create AccessDenied.cshtml.cs

---
+++ Pages/Admin/AccessDenied.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ECommerce.Pages.Admin
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Tu peux ajouter ici une logique personnalisée si besoin
+            // Par exemple, journaliser l'accès refusé ou récupérer des infos sur l'utilisateur
+        }
+    }
+}
