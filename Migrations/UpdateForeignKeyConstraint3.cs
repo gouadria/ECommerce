7f126d83e13a268e1d675c974758db0a1d0cf55f
gouadria <134183088+gouadria@users.noreply.github.com>
2025-04-21 01:41:20 +0200

    Create UpdateForeignKeyConstraint3.cs

---
+++ Migrations/UpdateForeignKeyConstraint3.cs
@@ -0,0 +1,71 @@
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommerce.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateForeignKeyConstraint3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Amount",
+                table: "Payments",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Payments",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Payments",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_UserId",
+                table: "Payments",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payments_AspNetUsers_UserId",
+                table: "Payments",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payments_AspNetUsers_UserId",
+                table: "Payments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Payments_UserId",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "Amount",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Payments");
+        }
+    }
+}
